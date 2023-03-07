bac3d9c7e1fd80caf94d2452f4d631e40c01b5a7
incava <ingi1118@naver.com>
2023-03-07 22:13:05 +0900

    feat : 상세페이지 내용 채움.map미구현

--- app/src/main/java/com/incava/notyourfault/DetailActivity.java
+++ app/src/main/java/com/incava/notyourfault/DetailActivity.java
@@ -3,22 +3,67 @@ package com.incava.notyourfault;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.telephony.PhoneNumberUtils;
+import android.text.util.Linkify;
+import android.widget.TextView;
+
+import com.naver.maps.map.NaverMapSdk;
+
+import java.util.Arrays;
+import java.util.Locale;
 
 /**
  * 상세 페이지를 보여주면서 전화, 지도, 홈페이지등 여러가지 정보를 볼 수 있다.
  */
 public class DetailActivity extends AppCompatActivity {
 
+    TextView tvTitle, tvLimitDay,tvNum, tvTarget, tvOper, tvSub, tvBus, tvTel, tvFax, tvHmpg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        findView(); //findView
+        setBundleOfData(); // bundle값
+    }
+
+    void findView(){//findView 정리.
+        tvTitle = findViewById(R.id.tv_title);
+        tvLimitDay = findViewById(R.id.tv_limitDay);
+        tvNum = findViewById(R.id.tv_num);
+        tvTarget = findViewById(R.id.tv_target);
+        tvOper = findViewById(R.id.tv_oper);
+        tvSub = findViewById(R.id.tv_sub);
+        tvBus = findViewById(R.id.tv_bus);
+        tvTel = findViewById(R.id.tv_tel);
+        tvFax = findViewById(R.id.tv_fax);
+        tvHmpg = findViewById(R.id.tv_hmpg);
+    }
+
+    void setBundleOfData(){
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
-        ShelterItem item = (ShelterItem) bundle.getSerializable("dataClass");
-        String name = item.fcltNm;
-        Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
+        ShelterItem item = (ShelterItem) bundle.getSerializable("dataClass");//MainActivity에서 받은 번들 풀기.
+        tvTitle.setText(item.fcltNm);
+        tvLimitDay.setText(item.etrPrdCn);
+        tvNum.setText(item.cpctCnt);
+        tvTarget.setText(item.etrTrgtCn);
+        tvOper.setText(item.operModeCn);
+        tvSub.setText(item.nrbSbwNm);
+        tvBus.setText(item.nrbBusStnNm);
+        tvTel.setText(PhoneNumberUtils.formatNumber(item.rprsTelno, Locale.getDefault().getCountry()));//지역에 맞게 하이픈 설정.
+        tvFax.setText(PhoneNumberUtils.formatNumber(item.fxno, Locale.getDefault().getCountry()));
+        tvHmpg.setText(item.hmpgAddr);
+        Linkify.addLinks(tvHmpg, Linkify.WEB_URLS);// 홈페이지 링크 걸기.
+        tvTel.setOnClickListener(v->telClick());
     }
+
+    void telClick(){
+        //전화번호 클릭시 전화할 수 있도록
+        Intent tt = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+tvTel.getText().toString()));
+        startActivity(tt);
+    }
+
 }
